11d013a921ab80ce88563a3f23ac3d73ff2f70aa
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2023-04-17 22:20:16 +0530

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-
-import './App.css';
+import "./App.css";
 
 import * as fcl from "@onflow/fcl";
 import * as types from "@onflow/types";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 /* import the ipfs-http-client and buffer libraries */
-import { create } from 'ipfs-http-client'
-import { Buffer } from 'buffer'
+import { create } from "ipfs-http-client";
+import { Buffer } from "buffer";
 
-const projectId = "2Jg0M9eybJ1dltguJTWfhnooRsr"
-const projectSecret = "0f4dbad34e18bf3c4cff4920302c71b7"
-const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
-
+const projectId = "2Jg0M9eybJ1dltguJTWfhnooRsr";
+const projectSecret = "0f4dbad34e18bf3c4cff4920302c71b7";
+const auth =
+  "Basic " + Buffer.from(projectId + ":" + projectSecret).toString("base64");
 
 const client = create({
-  host: 'ipfs.infura.io',
+  host: "ipfs.infura.io",
   port: 5001,
-  protocol: 'https',
+  protocol: "https",
   headers: {
-      authorization: auth,
-  }
-})
+    authorization: auth,
+  },
+});
 
-
-fcl.config()
+fcl
+  .config()
   .put("accessNode.api", "https://access-testnet.onflow.org")
-  .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn")
+  .put("discovery.wallet", "https://fcl-discovery.onflow.org/testnet/authn");
 
 function App() {
   const [user, setUser] = useState();
@@ -33,29 +32,29 @@ function App() {
 
   useEffect(() => {
     fcl.currentUser().subscribe(setUser);
-  }, [])
+  }, []);
   const logIn = () => {
     fcl.authenticate();
-  }
+  };
   const logOut = () => {
     fcl.unauthenticate();
-  }
+  };
   const mint = async () => {
     const added = await client.add(file);
     const hash = added.path;
 
     console.log(hash);
-  }
+  };
   return (
     <div className="App">
-      {user && user.addr ? <h1>{user.addr}</h1> : null }
+      {user && user.addr ? <h1>{user.addr}</h1> : null}
 
-        <h1> Hi MemeMint </h1>
-        <button onClick={() => logIn()}> Log In </button>
-        <button onClick={() => logOut()}>Log Out</button>
+      <h1 className="text-yellow-600 text-2xl"> Hi MemeMint </h1>
+      <button onClick={() => logIn()}> Log In </button>
+      <button onClick={() => logOut()}>Log Out</button>
 
-        <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-        <button onClick={() => mint()}>Mint HolidayNFT</button>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+      <button onClick={() => mint()}>Mint HolidayNFT</button>
     </div>
   );
 }
