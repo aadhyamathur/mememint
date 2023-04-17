d12f43e15b943eeac82ed6b81e878dd7007211f1
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2023-04-17 22:33:45 +0530

    landing page

---
+++ src/pages/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+function index() {
+  return (
+    <>
+      <div className="mt-[40px] sm:mt-[100px] justify-center items-center  flex-col">
+        <h3 className="justify-center items-center flex text-xl my-3 ">
+          Give your audience a voice{" "}
+          <img src="./speaker.svg" alt="speaker" className="h-7 mx-2" />
+        </h3>
+        <h1 className="text-[45px] font-bold text-center">
+          Add <span className="text-main">engagement </span>
+          to your content <br /> with on-chain
+          <span className="text-main"> Comments</span>
+        </h1>
+        <h2 className=" text-[18px]  mx-auto text-center my-4">
+          Easy to integrate comments section{" "}
+          <span className="text-main text-semibold">built on Lens</span>{" "}
+          Decentralized social graph.
+          <br /> Allow your users to utilize a user-owned social graph and
+          engage with your content.
+        </h2>
+        <a href={"/register"}>
+          <button className="m-6 mt-15 bg-main text-[#FEFFFE] border-0 px-5 py-3 rounded-md flex items-center mx-auto">
+            <img src="./chat.svg" alt="comments" className="mx-1 h-5" /> Add
+            Buzz to your site
+          </button>
+        </a>
+      </div>
+
+      <img
+        src="./demo1.png"
+        alt="demo"
+        className="w-[90vw] sm:w-[50%] rounded-lg  border-2 border-dashed mx-auto border-main my-20 mb-30"
+      />
+      <a href="https://www.lens.xyz" target="_blank" rel="noreferrer">
+        <div className="cursor-pointer mx-auto my-10 flex text-2xl bg-[#AAFE2C] text-[#01501E] justify-center items-center w-fit p-3 py-3 rounded-md">
+          <img src="./lens.svg" alt="lens" className="mx-3" />
+          Built on Lens Protocol{" "}
+        </div>
+      </a>
+    </>
+  );
+}
+
+export default index;
